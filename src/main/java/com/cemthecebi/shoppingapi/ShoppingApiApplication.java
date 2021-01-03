217cd02d7ccd2c526d3ff4abdc4f200fe61a23d2
Cem Berke Cebi <cemberke.cebi@trendyol.com>
2021-01-03 17:56:46 +0300

    first commit

---
+++ src/main/java/com/cemthecebi/shoppingapi/ShoppingApiApplication.java
@@ -0,0 +1,12 @@
+package com.cemthecebi.shoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApiApplication.class, args);
+	}
+}
